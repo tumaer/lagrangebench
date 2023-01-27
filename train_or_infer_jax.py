22f3ac6064c26602aa878873ffbbc08efb4bd95c
Artur Toshev <artur.toshev@tum.de>
2023-01-27 22:32:32 +0000

    add TGVCoarse to main file

--- train_or_infer_jax.py
+++ train_or_infer_jax.py
@@ -348,7 +348,7 @@ if __name__ == "__main__":
     elif args.dataset_name in ["WaterDrop", "WaterDropSample"]:
         particle_dimension = 2
         # node_in = 30, edge_in = 3
-    elif args.dataset_name in ["TGV_debug", "dataset_tgv_debug", "dataset_ut"]:
+    elif "tgv" in args.dataset_name or "TGV" in args.dataset_name:
         particle_dimension = 3
 
     # preprocessing allocate and update functions in the spirit of jax-md's
